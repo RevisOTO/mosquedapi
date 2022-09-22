d7a0ccb246be3657ded9826a36a6d0325b35505d
¨RevisOTO¨ <rafamosqueda.barragan12@gmail.com>
2022-09-21 21:09:09 -0500

    Tarea Express

--- Parcial 1/Server express/Express.js
+++ Parcial 1/Server express/Express.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors({origin:"http://localhost"}))
+
 app.get('/', function (req, res) {
+
+  res.sendFile("./static/index.html",{root:__dirname})
+})
+app.post('/', function (req, res) {
+  res.json({usuario:"rafael"})
   res.send('Hola soy un server de express')
 })
 
-app.post('/', function (req, res) {
-    res.send('Hola soy un server de express')
+app.use(function (req, res) {
+  res.status(404).sendFile("./static/404.html",{root:__dirname})
 })
-
 app.listen(8081)
